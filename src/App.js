f3b5829d200f6388425990f6ec511373ecfcb63a
Henrique Silva <henrique_mateus1994@hotmail.com>
2021-05-18 00:29:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import {React, useState} from 'react'
+
+import styled from 'styled-components';
+
+const Component = styled.div``
+
+const Title = styled.h1`
+  font-weight:bold;
+`
+const Input = styled.input`
+  width:400px;
+  height:40px;
+`
+const Total = styled.p`
+  font-weight:bold;
+`
 
 function App() {
+
+  const [number, setNumber] = useState(0)
+
+  const numero = Number(number)
+  console.log(typeof numero)
+  const handleInput = (e) =>{
+    setNumber( e.target.value)
+  }
+
+  const [percent, setPercent] = useState(0)
+
+  const porcento = Number(percent);
+
+  const handlePercent = (e) => {
+   
+    setPercent(e.target.value)
+
+  }
+
+  const gp = numero * porcento / 100;
+
+  const total = gp + numero
+
+ 
+  console.log(numero.length)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+      <Title>Calculadora de Gorjeta</Title>
+        <p>Quanto deu a conta?</p>
+      <Input type="number" value={numero}  placeholder="Digite o valor da conta." onChange={handleInput}/>
+        <p>Qual a porcentagem de gorjeta?</p>
+      <Input type="number" value={porcento}  placeholder="Digite o valor da conta." onChange={handlePercent}/>
+      <hr/>
+      
+      
+     {numero > 0 &&
+      <Component>
+        <p>Sub-total: R$ {numero}</p>
+        <p>Gorjeta({porcento}%): R$ {gp}</p>
+        <Total>Total: R$ {total}</Total>
+      </Component>
+      }
+   </>
   );
 }
 
